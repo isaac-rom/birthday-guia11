b957aeaad72ca87c09db9f7762be60434879f646
isaac-rom <141902516+isaac-rom@users.noreply.github.com>
2023-11-08 19:00:36 -0600

    init

--- App.js
+++ App.js
@@ -1,20 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, SafeAreaView, StatusBar, LogBox } from "react-native";
+import base64 from "react-native-base64";
+import Auth from "./src/components/Auth";
+import firebase from "./utils/firebase";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import ListBirthday from "./src/components/ListBirthday";
+
+function btoa(data) {
+  return new base64(data, "binary").toString("base64");
+}
+function atob(data) {
+  return new base64(data, "base64").toString("binary");
+}
+LogBox.ignoreAllLogs(["Setting a timer"]);
 
 export default function App() {
+  const [user, setUser] = useState(undefined);
+  useEffect(() => {
+    const auth = getAuth();
+    const listener = onAuthStateChanged(auth, async (user) => {
+      setUser(user);
+    });
+    return () => {
+      listener();
+    };
+  }, []);
+  if (user === undefined) return null;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <StatusBar barStyle="light-content" />
+    <SafeAreaView style={styles.background}>
+      {user ? <ListBirthday user={user} /> : <Auth />}
+    </SafeAreaView>
+    </>
   );
 }
-
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  background: {
+    backgroundColor: "#15212b",
+    height: "100%",
   },
 });
---
+++ utils/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBhK6eMFst87kb1zL2GSnsdLzveQL7MHAU",
+  authDomain: "birthday-30cc9.firebaseapp.com",
+  projectId: "birthday-30cc9",
+  storageBucket: "birthday-30cc9.appspot.com",
+  messagingSenderId: "614917079621",
+  appId: "1:614917079621:web:486d49552ce6d9bd18992c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
